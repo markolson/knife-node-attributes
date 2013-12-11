0f24a1bc294f99bfd59436fff15f23b569b9d8e9
Mark Olson <theothermarkolson@gmail.com>
2013-12-10 21:25:22 -0500

    first commit

---
+++ lib/knife/node/attributes.rb
@@ -0,0 +1,9 @@
+require "knife/node/attributes/version"
+
+module Knife
+  module Node
+    module Attributes
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/knife/node/attributes/version.rb
@@ -0,0 +1,7 @@
+module Knife
+  module Node
+    module Attributes
+      VERSION = "0.0.1"
+    end
+  end
+end
